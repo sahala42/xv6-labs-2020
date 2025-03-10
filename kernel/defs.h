0b914a516effe2ea83db58e74f9782591ccc490c
zst <zoushengtao2000@126.com>
2025-03-10 12:31:25 +0800

    完成实验Backtrace:运行用户程序函数，打印用户到内核的调用的栈地址

--- kernel/defs.h
+++ kernel/defs.h
@@ -80,6 +80,7 @@ int             pipewrite(struct pipe*, uint64, int);
 void            printf(char*, ...);
 void            panic(char*) __attribute__((noreturn));
 void            printfinit(void);
+void            zst_backtrace();  // 加上
 
 // proc.c
 int             cpuid(void);
